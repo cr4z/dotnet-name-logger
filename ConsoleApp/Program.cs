161bfa4f053c551cf3abd72045260617b926216b
cr4z <stevencr7zz@gmail.com>
2025-08-25 17:19:09 -0500

    Add solution base

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using NumberLoggerLibrary;
+
+NumberLoggerOptions options = new(100);
+NumberLogger logger = new(options);
+
+List<string> logs = logger.Run();
+
+Console.WriteLine(logs); // LINQ enumerate over
+
---
+++ NumberLoggerLibrary/NumberLogger.cs
@@ -0,0 +1,42 @@
+﻿namespace NumberLoggerLibrary
+{
+    public class NumberLoggerOptions(int upperBounds)
+    {
+        public int UpperBounds { get; set; } = upperBounds;
+    }
+
+
+    public class NumberLogger(NumberLoggerOptions options)
+    {
+        public int UpperBounds { get; set; } = options.UpperBounds;
+
+        public List<string> Run()
+        {
+            List<string> res = [];
+            int logs = 0;
+
+            while (logs < UpperBounds)
+            {
+                logs++;
+                string msg = logs.ToString();
+
+                if ((logs % 3) == 0)
+                {
+                    msg = "Steven";
+                }
+                if ((logs % 5) == 0)
+                {
+                    msg = "Bennett";
+                }
+                if ((logs % 3 == 0) && (logs % 5 == 0))
+                {
+                    msg = "Steven Bennett";
+                }
+
+                res.Add(msg);
+            }
+
+            return res;
+        }
+    }
+}
